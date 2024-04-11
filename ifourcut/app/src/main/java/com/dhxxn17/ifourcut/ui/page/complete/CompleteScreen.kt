3d9f0a652d911cf7b53ab05b7778c50980fb96a2
xxria17 <xxrcy32@gmail.com>
2024-04-11 22:50:48 +0900

    [Update] 공유하기, 저장하기 기능 추가

--- ifourcut/app/src/main/java/com/dhxxn17/ifourcut/ui/page/complete/CompleteScreen.kt
+++ ifourcut/app/src/main/java/com/dhxxn17/ifourcut/ui/page/complete/CompleteScreen.kt
@@ -1,5 +1,19 @@
 package com.dhxxn17.ifourcut.ui.page.complete
 
+import android.app.Activity
+import android.app.DownloadManager
+import android.content.ContentValues
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Base64
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -31,10 +45,18 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.FileProvider
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.dhxxn17.ifourcut.R
 import com.dhxxn17.ifourcut.ui.base.BaseScreen
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class CompleteScreen (
     private val navController: NavController,
@@ -43,6 +65,8 @@ class CompleteScreen (
     @Composable
     override fun CreateContent() {
         val viewModel: CompleteViewModel = hiltViewModel()
+        val context = LocalContext.current
+
 
         Box(
             modifier = Modifier
@@ -111,7 +135,10 @@ class CompleteScreen (
                 ) {
                     Button(
                         onClick = {
-
+                            viewModel.state.image.value()?.let {
+                                saveBitmapImage(it, context)
+                                Toast.makeText(context, "저장되었습니다.", Toast.LENGTH_SHORT).show()
+                            }
                         },
                         modifier = Modifier
                             .weight(1f)
@@ -136,7 +163,9 @@ class CompleteScreen (
 
                     Button(
                         onClick = {
-
+                            viewModel.state.image.value()?.let {
+                                shareImage(it, context.findActivity())
+                            }
                         },
                         modifier = Modifier
                             .weight(1f)
@@ -162,6 +191,98 @@ class CompleteScreen (
 
     }
 
+    private fun shareImage(bitmap: Bitmap, activity: Activity) {
+        val shareIntent = Intent().apply{
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_STREAM, getImageUri(activity, bitmap))
+            type = "image/*"
+        }
+        activity.startActivity(Intent.createChooser(shareIntent, "이미지 공유하기"))
+    }
 
+    private fun getImageUri(activity: Activity, bitmap: Bitmap): Uri {
+        val cachePath = File(activity.cacheDir, "images")
+        cachePath.mkdirs()
+        val fileName = generateFileName()
+        val stream = FileOutputStream("$cachePath/${fileName}.png")
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        stream.close()
+        val imagePath = File(activity.cacheDir, "images").absolutePath
+        val imageFile = File(imagePath, "${fileName}.png")
+        return FileProvider.getUriForFile(
+            activity,
+            "${activity.packageName}.provider",
+            imageFile
+        )
+    }
+
+    fun Context.findActivity(): Activity {
+        var context = this
+        while (context is ContextWrapper) {
+            if (context is Activity) return context
+            context = context.baseContext
+        }
+        throw IllegalStateException("no activity")
+    }
+
+
+    private fun generateFileName(): String {
+        val timestamp = SimpleDateFormat("ddHHmm", Locale.getDefault()).format(Date())
+        val randomString = (1..6).map { ('a'..'z').random() }.joinToString("")
+        return "image_${timestamp}$randomString.png"
+    }
+
+    private fun saveBitmapImage(bitmap: Bitmap, context: Context) {
+        val timestamp = System.currentTimeMillis()
+        val contentResolver = context.findActivity().contentResolver
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        values.put(MediaStore.Images.Media.DATE_ADDED, timestamp)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.DATE_TAKEN, timestamp)
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + generateFileName())
+            values.put(MediaStore.Images.Media.IS_PENDING, true)
+            val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            if (uri != null) {
+                try {
+                    val outputStream = contentResolver.openOutputStream(uri)
+                    if (outputStream != null) {
+                        try {
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                            outputStream.close()
+                        } catch (e: Exception) {
+                            Log.e("CompleteScreen", "saveBitmapImage: ", e)
+                        }
+                    }
+                    values.put(MediaStore.Images.Media.IS_PENDING, false)
+                    contentResolver.update(uri, values, null, null)
+
+                } catch (e: Exception) {
+                    Log.e("CompleteScreen", "saveBitmapImage: ", e)
+                }
+            }
+        } else {
+            val imageFileFolder = File(Environment.getExternalStorageDirectory().toString() + '/' + generateFileName())
+            if (!imageFileFolder.exists()) {
+                imageFileFolder.mkdirs()
+            }
+            val mImageName = "$timestamp.png"
+            val imageFile = File(imageFileFolder, mImageName)
+            try {
+                val outputStream: OutputStream = FileOutputStream(imageFile)
+                try {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                    outputStream.close()
+                } catch (e: Exception) {
+                    Log.e("CompleteScreen", "saveBitmapImage: ", e)
+                }
+                values.put(MediaStore.Images.Media.DATA, imageFile.absolutePath)
+                contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+            } catch (e: Exception) {
+                Log.e("CompleteScreen", "saveBitmapImage: ", e)
+            }
+        }
+    }
 
 }
